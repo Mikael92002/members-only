aa358039fcae54b14eac964dcc57409d40852942
Mikael <174565360+Mikael92002@users.noreply.github.com>
2026-02-04 19:29:49 -0600

    Re-added setup file for prod

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# set -e
+
+# build client
+cd client/members-only
+npm ci
+npm run build
+
+# install server deps
+cd ../../server
+npm ci
+
+# ensure NODE_ENV=production for serving the built static files
+export NODE_ENV=${NODE_ENV:-production}
+
+# start server
+node app.js
